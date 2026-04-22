91b158ea54b7c38861874d5377cc87435948e024
Giuliano Franca <giulianofranca@outlook.com>
2026-04-22 20:07:42 -0300

    feat: Update content to print and add content.py with the content

---
+++ app/content.py
@@ -0,0 +1,1 @@
+CONTENT = "Working content only out here!"
--- app/initial_file.py
+++ app/initial_file.py
@@ -1,6 +1,9 @@
+from app import content
+
 def main():
     print("Hello World!")
     print("Hello Perforce!")
+    print(content.CONTENT)
 
 if __name__ in '__main__':
     main()
